55694eaf7eb9de21da6fee58c3168d2c97475485
Ranuj Chaudhary <ranujchoudhary@gmail.com>
2025-04-10 13:14:48 +0530

    refactor/authreducer dashboard

--- dashboard/src/utils/utils.js
+++ dashboard/src/utils/utils.js
@@ -16,3 +16,25 @@ export const throttle = function throttle(func, limit) {
     }
   };
 };
+
+
+export const  handleAxiosError = (error, rejectWithValue) => {
+  if (error.response) {
+    // HTTP Errors (4xx, 5xx)
+    console.error(`HTTP Error ${error.response.status}:`, error.response.data);
+
+    return rejectWithValue({
+      message: error.response.data.message || 'Server error',
+    });
+  } else if (error.request) {
+    console.error('Network error: No response received.');
+    // Network Errors (No response)
+    return rejectWithValue({
+      message: 'Network error. Please check your connection.',
+    });
+  } else {
+    console.error('Unexpected error:', error.message);
+    // Other Errors (e.g., syntax errors)
+    return rejectWithValue({ message: 'Unexpected error occurred' });
+  }
+}
